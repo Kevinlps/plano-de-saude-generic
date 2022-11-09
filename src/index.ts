6ef7f4e2a1f8a00c8c2771313df6a4f330345d8f
Kevin <kevinlopes3012@gmail.com>
2022-11-08 22:24:17 -0400

    .

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ import { Exame } from './models/entities/Exame'
 const run = async () => {
   await createTables()
 
-  //const newBeneficiario = new Beneficiario ('Bob','Rua:casas n:2','999999999',new Date(21/2/3))
-  //const dao1 = new GenericDAO(Beneficiario)
-  //const savedUser = await dao1.save(newBeneficiario)
-  //console.log(savedUser)
-
+  const newBeneficiario = new Beneficiario ('Bob','Rua:casas n:2','999999999',new Date(21/2/3))
   const dao1 = new GenericDAO(Beneficiario)
-  const busca = await dao1.findById(1)
+  const savedUser = await dao1.save(newBeneficiario)
+  console.log(savedUser)
+
+  const dao2 = new GenericDAO(Beneficiario)
+  const busca = await dao2.findById(1)
   console.log(busca)
 }
 
